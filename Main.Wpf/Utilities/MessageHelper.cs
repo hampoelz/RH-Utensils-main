f138754160a5807f46dceb2fcf53c828099894ba
Rene Hampölz <service@hampoelz.net>
2019-08-03 12:31:13 +0200

    Bug fixes, Optimizations, New key forward system, New menu system

--- Main.Wpf/Utilities/MessageHelper.cs
+++ Main.Wpf/Utilities/MessageHelper.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MaterialDesignThemes.Wpf;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Runtime.InteropServices;
@@ -26,7 +27,7 @@ namespace Main.Wpf.Utilities
             {
                 dwData = IntPtr.Zero,
                 lpData = _stringMessageBuffer,
-                cbData = msg.Length * 2//Number of bytes required for marshalling this string as a series of unicode characters
+                cbData = msg.Length * 2 //Number of bytes required for marshalling this string as a series of unicode characters
             };
             IntPtr _copyDataBuff = IntPtrAlloc(_copyData);
 
@@ -39,9 +40,9 @@ namespace Main.Wpf.Utilities
 
         public static void SendDataBroadcastMessage(string msg)
         {
-            foreach ((Process proc, int id) p in MainWindow.backgroundProcesses)
+            foreach ((Process proc, int id) in MainWindow.backgroundProcesses)
             {
-                SendDataMessage(p.proc, msg);
+                SendDataMessage(proc, msg);
             }
         }
 
@@ -74,16 +75,20 @@ namespace Main.Wpf.Utilities
             return IntPtr.Zero;
         }
 
+        private static bool _isHandling;
+
         private static async void Handle(string msg)
         {
-            await Task.Delay(50);
+            while (_isHandling) await Task.Delay(100);
+
+            _isHandling = true;
 
             if (msg.StartsWith("set Color"))
             {
                 await XmlHelper.SetString(Config.File, "config/color", ExtractQuote(msg));
                 Config.Informations.Extension.Color = ExtractQuote(msg);
             }
-            else if (msg.StartsWith("set tmpColor"))
+            else if (msg.StartsWith("set tmp Color"))
             {
                 Config.Informations.Extension.Color = ExtractQuote(msg);
             }
@@ -92,12 +97,16 @@ namespace Main.Wpf.Utilities
                 await XmlHelper.SetString(Config.File, "config/favicon", ExtractQuote(msg));
                 Config.Informations.Extension.Favicon = ExtractQuote(msg);
             }
+            else if (msg.StartsWith("set tmp Favicon"))
+            {
+                Config.Informations.Extension.Favicon = ExtractQuote(msg);
+            }
             else if (msg.StartsWith("set WindowHeight"))
             {
                 await XmlHelper.SetString(Config.File, "config/height", ExtractQuote(msg));
                 Config.Informations.Extension.WindowHeight = int.Parse(ExtractQuote(msg));
             }
-            else if (msg.StartsWith("set tmpWindowHeight"))
+            else if (msg.StartsWith("set tmp WindowHeight"))
             {
                 Config.Informations.Extension.WindowHeight = int.Parse(ExtractQuote(msg));
             }
@@ -106,40 +115,99 @@ namespace Main.Wpf.Utilities
                 await XmlHelper.SetString(Config.File, "config/width", ExtractQuote(msg));
                 Config.Informations.Extension.WindowWidth = int.Parse(ExtractQuote(msg));
             }
-            else if (msg.StartsWith("set tmpWindowWidth"))
+            else if (msg.StartsWith("set tmp WindowWidth"))
             {
                 Config.Informations.Extension.WindowWidth = int.Parse(ExtractQuote(msg));
             }
             else if (msg.StartsWith("set selectionIndex"))
             {
-                var index = int.Parse(ExtractQuote(msg));
+                if (int.TryParse(ExtractQuote(msg), out var index) && index - 1 >= 0 && index <= Config.Menu.Sites.Count)
+                {
+                    if (Config.Menu.Sites[index - 1].Space) index = 1;
 
-                await XmlHelper.SetString(Config.File, "config/selectionIndex", (index).ToString());
-
-                await MenuHelper.SelectMenuItemAsync(index - 1).ConfigureAwait(false);
+                    await MenuHelper.SelectMenuItemAsync(index - 1);
+                    await XmlHelper.SetString(Config.File, "config/selectionIndex", (index).ToString());
+                }
+                else
+                {
+                    await MenuHelper.SelectMenuItemAsync(0);
+                }
             }
-            else if (msg.StartsWith("add Site"))
+            else if (msg.StartsWith("set tmp selectionIndex"))
+            {
+                if (int.TryParse(ExtractQuote(msg), out var index) && index - 1 >= 0 && index <= Config.Menu.Sites.Count)
+                {
+                    if (Config.Menu.Sites[index - 1].Space) index = 1;
+
+                    await MenuHelper.SelectMenuItemAsync(index - 1);
+                }
+                else
+                {
+                    await MenuHelper.SelectMenuItemAsync(0);
+                }
+            }
+            else if (msg.StartsWith("add tmp Site"))
             {
                 var title = ExtractQuotes(msg)[0];
                 var icon = ExtractQuotes(msg)[1];
                 var path = ExtractQuotes(msg)[2];
                 var startArguments = ExtractQuotes(msg)[3];
 
-                //To-Do
+                var sites = Config.Menu.Sites;
+
+                if (title == "null" || path == "null") sites.Insert(sites.Count - 3, new MenuItem() { Space = true });
+                else sites.Insert(sites.Count - 3, new MenuItem { Title = title, Icon = Enum.TryParse(icon, out PackIconKind Icon) ? Icon : PackIconKind.Application, Path = path, StartArguments = startArguments });
+
+                await Config.Menu.SetSites(sites);
             }
-            else if (msg.StartsWith("update Site"))
+            else if (msg.StartsWith("update tmp Site"))
             {
-                var index = int.Parse(ExtractQuotes(msg)[0]);
+                var sites = Config.Menu.Sites;
                 var title = ExtractQuotes(msg)[1];
                 var icon = ExtractQuotes(msg)[2];
 
-                //To-Do
-            }
-            else if (msg.StartsWith("remove Site"))
-            {
-                var index = int.Parse(ExtractQuotes(msg)[0]);
+                var _currentIndex = Pages.Menu.ListViewMenu.SelectedIndex;
 
-                //To-Do
+                if (int.TryParse(ExtractQuotes(msg)[0], out var index) && index - 1 >= 0 && index - 1 <= Config.Menu.Sites.Count - 3)
+                {
+                    sites[index - 1].Title = title;
+                    sites[index - 1].Icon = Enum.TryParse(icon, out PackIconKind Icon) ? Icon : PackIconKind.Application;
+
+                    if (index - 1 != _currentIndex)
+                    {
+                        var path = ExtractQuotes(msg)[3];
+                        var startArguments = ExtractQuotes(msg)[4];
+
+                        sites[index - 1].Path = path;
+                        sites[index - 1].StartArguments = startArguments;
+                    }
+                }
+
+                await Config.Menu.SetSites(sites);
+            }
+            else if (msg.StartsWith("remove tmp Site"))
+            {
+                var sites = Config.Menu.Sites;
+
+                var _currentIndex = Pages.Menu.ListViewMenu.SelectedIndex;
+
+                if (int.TryParse(ExtractQuote(msg), out var index) && index - 1 >= 0 && index - 1 <= Config.Menu.Sites.Count - 3 && index - 1 != _currentIndex)
+                {
+                    sites.RemoveAt(index - 1);
+                }
+
+                await Config.Menu.SetSites(sites);
+
+                if (index - 1 == 0)
+                {
+                    _currentIndex = _currentIndex - index - 1;
+                }
+                else if (index - 1 < _currentIndex)
+                {
+                    _currentIndex--;
+                }
+
+                MenuHelper.MoveCursorMenu(_currentIndex);
             }
             else if (msg.StartsWith("change SettingProperty"))
             {
@@ -160,11 +228,11 @@ namespace Main.Wpf.Utilities
             }
             else if (msg.StartsWith("open File"))
             {
-                if (!(Application.Current.MainWindow is MainWindow mw)) return;
-
                 ExtensionsManager.FileToOpen = ExtractQuote(msg);
                 SendDataBroadcastMessage(msg);
             }
+
+            _isHandling = false;
         }
 
         private static string ExtractQuote(string str)
